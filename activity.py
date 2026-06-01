516622c6b42c32e6511541c95c799cee9d66c3b2
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-06-01 19:08:12 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,19 @@
+print ("hello, world")
+
+print ("come outside", 6)
+
+print ("where are you /n")
+
+print ("hi,what is your name",end="*")
+
+rope=67
+
+print (rope)
+
+hope="hello"
+
+print (hope)
+
+name=input("what is your name") 
+
+print (name)
