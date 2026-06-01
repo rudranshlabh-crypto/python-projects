fc31c2155fa5138dd65bc68fae750e12f3041149
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-06-01 19:27:28 +0530

    Add files via upload

---
+++ storing  birthdays.py
@@ -0,0 +1,19 @@
+friend1="darhshit"
+
+print (friend1)
+
+friend2="subyasachi"
+
+print (friend2)
+
+friend3="rehan"
+
+print (friend3)
+
+friend4="arsh"
+
+print (friend4)
+
+friend5="aryan"
+
+print (friend5)
