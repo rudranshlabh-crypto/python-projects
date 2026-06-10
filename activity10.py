7155a899844200b6edd42560cad070dfaddf11cb
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-06-10 20:03:02 +0530

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,23 @@
+a=345
+b=0
+c=-567
+
+if a and b and c:
+    print ("the numbers are greater than zero")
+else:
+    print ("one of them is zero")
+
+a=-10
+b=0
+c=10
+d=20
+
+if a > 0 or b > 0:
+    print ("they are greater than 0.")
+else:
+    print ("they are smaller than 0.")
+
+if c > 0 or d > 0:
+    print ("the numbers is positive.")
+else:
+    print ("any one of them is a negative number.")
---
+++ activity11.py
@@ -0,0 +1,18 @@
+a=9
+b=67
+c=67
+print (not(a==b))
+print (not(b==c))
+
+a="codingal"
+b="curious jr."
+if not (a==b):
+    print (a, "and", b, "are different from each other")
+else:
+    print ("both", a, b, "are same")
+
+a=int (input("enter any number"))
+if (not(a % 2 == 0)):
+    print ("it is odd")
+else:
+    print ("it is even 0")
