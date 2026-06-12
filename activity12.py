3304d8bef92f2c76fc863bf30940056db4f91412
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-06-12 19:04:39 +0530

    Add files via upload

---
+++ activity12.py
@@ -0,0 +1,13 @@
+a=[1,2,3,4,5,6,7,8,9]
+b=[1,2,3,4,5,6,7,8,9]
+c=a
+
+print (a is c)
+print (a is b)
+print (b is c)
+print (a is not c)
+print (a is not b)
+print (a in c)
+print (a not in b)
+print (a in b)
+print (a not in c)
---
+++ activity13.py
@@ -0,0 +1,18 @@
+sentence="hello!how are you?"
+print ("hello" in sentence)
+print ("python" in sentence)
+
+x=-5
+
+if  type (x) is int:
+    print ("x is a number")
+else:
+    print ("x is a alphabet")
+
+x="qwerty"
+y="qwerty"
+
+if type (x) == type (y):
+    print ("x and y are SAME.")
+else:
+    print ("x and y are DIFFERENT.")
