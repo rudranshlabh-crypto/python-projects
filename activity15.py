c5773b6b6e26590e5a3456bf4636953206190725
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-06-15 18:59:38 +0530

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,16 @@
+a=12
+b=23
+c=34
+d=45
+e=(a+b)*c/d
+print (type (e))
+
+name = "darshit"
+age = 100000
+
+if name == "darshit" or name == "subyasachi":
+    print ("he is my friend")
+elif age == 100 or age >= 10:
+    print ("his age is above 14")
+else:
+    print ("his is not my friend and he is below 10")
---
+++ activity16.py
@@ -0,0 +1,7 @@
+num1=int (input("enter the first number"))
+num2=int (input("enter the second number"))
+
+if num1 % num2 == 0:
+    print ("the first num is divisible by second num")
+else:
+    print ("the first num is not divisible by second num")
