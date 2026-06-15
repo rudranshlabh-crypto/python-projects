d034eff46b9ffa8547c8b414bc6eb9e73e2856a9
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-06-15 19:13:43 +0530

    Add files via upload

---
+++ activity17.py
@@ -0,0 +1,11 @@
+mean1=39
+wrong_number=40
+correct_number=90
+
+total_numbers=40
+
+wrong_sum = total_numbers * mean1
+correct_sum = wrong_sum-wrong_number+correct_number
+
+mean2=correct_sum/total_numbers
+print ("the correct mean is", mean2)
