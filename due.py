e8da24dbd6b44542067d14ee185c6f73acc35bf9
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-07-15 18:01:21 +0530

    Add files via upload

---
+++ due.py
@@ -0,0 +1,6 @@
+total_bill = float(input("Enter the total bill amount: "))
+amount_paid = float(input("Enter the amount paid: "))
+
+due_amount = total_bill - amount_paid
+
+print("Remaining due amount:", due_amount)
