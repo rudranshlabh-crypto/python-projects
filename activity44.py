c853543d058d7ae7bddb8a563ce600254a6a29e8
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-07-22 18:58:45 +0530

    Add files via upload

---
+++ activity44.py
@@ -0,0 +1,17 @@
+tuple=(2, 3.5, "tuple X")
+print (tuple)
+
+tuple=(2,5,7,9)
+print (tuple)
+
+tuple=(50,60,80,50,90,99)
+print (tuple.count(50))
+
+tuple=tuplex + (9,)
+print (tuple)
+
+tuple=(1,2,3,2,1)
+silce=tuple[1:3]
+print (silce)
+_slice=tuple[:5]
+print (_slice)
