04ed0ca7fc82844dcc2254d2a0c87abe17d71230
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-07-24 18:54:50 +0530

    Add files via upload

---
+++ activity46.py
@@ -0,0 +1,10 @@
+my_dict=("codingal":2, "is":2, "best":2, "for":2, "coding":1)
+print(my_dict)
+
+t=2
+
+res=0
+for i in my_dict:
+    if my_dict [key]==t:
+        res=res+1
+print("frequence of 2 is ", res)
