5a092bba96399d0d42308fb3f7f1ab2f79565330
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-07-24 19:10:44 +0530

    Add files via upload

---
+++ activity47.py
@@ -0,0 +1,7 @@
+country_code={"India":"0091", "australia":"0025", "america":"0001", "china":"0067"}
+print("country code of india is:")
+print(country_code.get("india"))
+print("country code of japan is:")
+print(country_code.get("japan"))
+print("country code of china is:")
+print(country_code.get("china"))
