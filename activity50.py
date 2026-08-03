1a972440ca73c24af172b885b4d61fd4506d8de3
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-08-03 19:14:33 +0530

    Add files via upload

---
+++ activity50.py
@@ -0,0 +1,8 @@
+class vehical:
+    
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+modelx=vehical(500,120)
+print (modelx.max_speed)
+print (modelx.mileage)
