a74a7ceaccf052d91edf5d930de51420bb27f9af
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-08-05 17:22:58 +0530

    Add files via upload

---
+++ pet.py
@@ -0,0 +1,12 @@
+class pets:
+    def__init__(self,species,name,age):
+        self.species=species
+        self.name=name
+        self.age=age
+
+doggie=pets("doggie",20)
+cuttie pie=pets("cuttie pie", 15)
+print ("doggie is a", doggie.species)
+print ("cuttie pie is a", cuttie pie.species)
+print (doggie.name, " is ", doggie.age, "year old")
+print (cuttie pie.name, " is ", cuttie pie.age, "year old")
