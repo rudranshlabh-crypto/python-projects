2871c8d50d1af4a23f4fa51fce48452e9400d849
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-08-05 19:27:10 +0530

    Add files via upload

--- activity53.py
+++ activity53.py
@@ -1,1 +1,12 @@
+class employee:
 
+     def __init__(self):
+        print ("employee created")
+    def __del__(self):
+        print ("employee deleted")
+    def createobj():
+        print ("creating object")
+        obj=employee()
+        return obj
+obj=createobj()
+print ("program end")
