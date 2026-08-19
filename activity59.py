38161581911458ef8d6fb8f092f12b2ccbb4656a
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-08-19 19:02:34 +0530

    Add files via upload

--- activity59.py
+++ activity59.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 screen=pygame.display.set_mode((400,300))
+pygame.draw.circle(window,(0,200,55),(300,200),3)
 done=False
 
 while not done:
@@ -10,3 +11,4 @@ while not done:
             done=True
     pygame.draw.rect(screen,(0,125,255),pygame.Rect,(30,30,60,60))
     pygame.display.flip()
+
