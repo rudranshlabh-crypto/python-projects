da5e669614178d15e63fe8f1fae541b60a6c7b12
rudranshlabh-crypto <rudransh.labh@gmail.com>
2026-08-21 16:42:23 +0530

    Add files via upload

--- test3.py
+++ test3.py
@@ -1,6 +1,5 @@
 pythonclass RomanConverter:
     def __init__(self):
-        # Define the mapping of integer values to Roman numerals in descending order
         self.val_map = [
             (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
             (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
@@ -14,7 +13,6 @@ pythonclass RomanConverter:
             
         roman_numeral = ""
         
-        # Loop through the values and build the string
         for value, symbol in self.val_map:
             while num >= value:
                 roman_numeral += symbol
@@ -22,12 +20,10 @@ pythonclass RomanConverter:
                 
         return roman_numeral
 
-# Example usage:
 if __name__ == "__main__":
     converter = RomanConverter()
     
     test_numbers = [1, 4, 9, 58, 1994, 3999]
     for number in test_numbers:
-        # Using .format() with positional arguments to avoid curly braces in code
         result = converter.int_to_roman(number)
         print("%d -> %s" % (number, result))
